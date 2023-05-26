202d0efcce8f6b6db552a4b6a694e5410e1aff9e
J <jpham26@gmail.com>
2023-05-25 17:34:24 -0700

    newspaper app

--- django_project/settings.py
+++ django_project/settings.py
@@ -33,6 +33,7 @@ INSTALLED_APPS = [
     # Local
     "accounts",
     "pages",
+    "articles",
 ]
 
 MIDDLEWARE = [
@@ -101,7 +102,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "America/Los_Angeles"
 
 USE_I18N = True
 
